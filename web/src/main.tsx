589a8b312c2e27459271339f290788a7d80b9d73
root <root@debian-s-2vcpu-4gb-amd-sgp1-01>
2026-02-01 03:27:16 +0000

    daily tech thing

--- web/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { StrictMode } from 'react/client'
-import App from './App'
-import './index.css'
-
-createInertiaApp(App).mount('#app', document.getElementById('root')!)
-export default defineConfig({
-  plugins: [react()],
-})
